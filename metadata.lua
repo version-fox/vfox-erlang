91b4943bea2d532c6abfa9fe2b5cb21241a6ec64
yeshan.ye <yeshan.ye@alibaba-inc.com>
2025-07-15 01:58:22 +0800

    chore: Update Erlang plugin version to 1.2.0 and remove .tool-versions file

--- metadata.lua
+++ metadata.lua
@@ -5,7 +5,7 @@ PLUGIN = {}
 --- Plugin name
 PLUGIN.name = "erlang"
 --- Plugin version
-PLUGIN.version = "1.1.0"
+PLUGIN.version = "1.2.0"
 --- Plugin homepage
 PLUGIN.homepage = "https://github.com/version-fox/vfox-erlang"
 --- Plugin license, please choose a correct license according to your needs.
